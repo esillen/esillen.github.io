2f876d8603e3383bc47ee4c215e0c16f246b1116
Erik Sillén <erik.sillen.1990@gmail.com>
2019-03-07 15:19:48 +0100

    Version 1 of the site

---
+++ assets/js/fancy_images.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+	/* This is basic - uses default settings */
+
+	$("a#single_image").fancybox( {
+		titlePosition : 'inside'
+	});
+
+	/* Using custom settings */
+
+	$("a#inline").fancybox({
+		'hideOnContentClick': true
+	});
+
+	/* Apply fancybox to multiple items */
+
+	$("a.group").fancybox({
+		'transitionIn'	:	'elastic',
+		'transitionOut'	:	'elastic',
+		'speedIn'		:	600,
+		'speedOut'		:	200,
+		'overlayShow'	:	false
+
+	});
+
+});
